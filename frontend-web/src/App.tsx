be2225c4d686e6a7e0f80343caca56ea7f2d0743
Tsoympet <tsoympet7@gmail.com>
2025-11-20 22:54:34 +0200

    Add files via upload

---
+++ frontend-web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div style={{ padding: 16 }}>
+      <h1>OracleSuite Web</h1>
+      <p>Skeleton dashboard. Backend: http://localhost:8000/api</p>
+    </div>
+  );
+};
+
+export default App;
